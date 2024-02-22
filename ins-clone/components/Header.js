89da85e8542f46dddea4e987e5cc3525d138f4a8
KAIXIN ZHANG <145351389+KevinMonash2022@users.noreply.github.com>
2024-02-22 23:24:24 +1100

    4. Add search bar to the header component

--- ins-clone/components/Header.js
+++ ins-clone/components/Header.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Image from 'next/image'
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
+
 
 export default function Header() {
-  return (
-    <div>
-        {/* left */}
-
-        
+    return (
         <div className="flex items-center justify-between max-w-6xl">
+            {/* Left */}
+
             {/* instagram display on large screen and hidden on the small screen */}
             <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
                 <Image
@@ -15,7 +15,7 @@ export default function Header() {
                     layout="fill"
                     className='object-contain'
                 />
-                
+
             </div>
 
             {/* icon display on small screen and hidden on the large screen */}
@@ -25,21 +25,20 @@ export default function Header() {
                     layout="fill"
                     className='object-contain'
                 />
-                
+
             </div>
 
+            {/* Middle */}
+            <div className="relative mt-1">
+                <div className="absolute top-2 left-2">
+                    <MagnifyingGlassIcon className='h-5 text-gray-500' />
+                </div>
+                <input type="text" placeholder='Search' className='bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md' />
+            </div>
+
+            {/* Right */}
             <h1>Right sides</h1>
 
         </div>
-
-        
-             
-        
-
-        {/* Middle */}
-
-        {/* Right */}
-
-    </div>
-  )
+    )
 }
